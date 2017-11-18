a787908ab35979d6eed4ca2236c9199b77ae03ed
sugyan <sugi1982@gmail.com>
2017-11-18 20:19:00 +0900

    use cron job, datastore

--- app.go
+++ app.go
@@ -1,7 +1,10 @@
 package app
 
 import (
+	"bytes"
+	"context"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"strings"
 
@@ -11,6 +14,7 @@ import (
 	"github.com/sugyan/shogi/logic/problem/generator"
 	"github.com/sugyan/shogi/logic/problem/solver"
 	"google.golang.org/appengine"
+	"google.golang.org/appengine/datastore"
 	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/urlfetch"
 )
@@ -33,10 +37,11 @@ func init() {
 	server := &server{
 		config: config,
 	}
-	http.HandleFunc("/", server.handler)
+	http.HandleFunc("/callback", server.botHandler)
+	http.HandleFunc("/generate", server.generateHandler)
 }
 
-func (s *server) handler(w http.ResponseWriter, r *http.Request) {
+func (s *server) botHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 	bot, err := linebot.New(
 		s.config.ChannelSecret, s.config.ChannelAccessToken,
@@ -63,29 +68,43 @@ func (s *server) handler(w http.ResponseWriter, r *http.Request) {
 		case linebot.EventTypeMessage:
 			if message, ok := event.Message.(*linebot.TextMessage); ok {
 				var (
-					problemType  = generator.ProblemType1
+					problemType  generator.Problem
 					replyMessage linebot.Message
 				)
-				if strings.HasPrefix(message.Text, "3手") {
+				switch {
+				case strings.HasPrefix(message.Text, "1手詰"):
+					problemType = generator.ProblemType1
+				case strings.HasPrefix(message.Text, "3手詰"):
 					problemType = generator.ProblemType3
 				}
-				problem := generator.Generate(problemType)
-				answer, err := solver.Solve(problem)
-				if err != nil || len(answer) != problemType.Steps() {
-					replyMessage = linebot.NewTextMessage("生成に失敗しました\xf0\x9f\x98\xa9")
-				} else {
-					path := strings.Replace(csa.InitialState2(problem), "\n", "/", -1)
-					imageURL := fmt.Sprintf("https://shogi-img.appspot.com/%s/simple.png", path)
-					text := fmt.Sprintf("%d手詰の問題です！", problemType.Steps())
-					replyMessage = linebot.NewTemplateMessage(
-						text+" LINEアプリでご覧ください",
-						linebot.NewButtonsTemplate(
-							imageURL, "", text,
-							linebot.NewURITemplateAction("画像URL", imageURL),
-							linebot.NewPostbackTemplateAction("正解を見る", fmt.Sprintf("正解は…\n%s です！", strings.Join(answer, " ")), ""),
-						),
-					)
+				if problemType == nil {
+					return
 				}
+				problemEntity, err := fetchProblem(ctx, problemType)
+				if err != nil {
+					log.Errorf(ctx, "failed to fetch problem: %v", err)
+					return
+				}
+				problem, err := csa.Parse(bytes.NewBufferString(problemEntity.State))
+				if err != nil {
+					log.Errorf(ctx, "failed to parse problem string: %v", err)
+					return
+				}
+				path := strings.Replace(csa.InitialState2(problem), "\n", "/", -1)
+				imageURL := fmt.Sprintf("https://shogi-img.appspot.com/%s/simple.png", path)
+				text := fmt.Sprintf("%d手詰の問題です！", problemType.Steps())
+				replyMessage = linebot.NewTemplateMessage(
+					text+" LINEアプリでご覧ください",
+					linebot.NewButtonsTemplate(
+						imageURL, "", text,
+						linebot.NewURITemplateAction("画像URL", imageURL),
+						linebot.NewPostbackTemplateAction(
+							"正解を見る",
+							fmt.Sprintf("正解は…\n%s です！", strings.Join(problemEntity.Answer, " ")),
+							"",
+						),
+					),
+				)
 				_, err = bot.ReplyMessage(event.ReplyToken, replyMessage).WithContext(ctx).Do()
 				if err != nil {
 					log.Errorf(ctx, "failed to reply message: %v", err.Error())
@@ -99,4 +118,96 @@ func (s *server) handler(w http.ResponseWriter, r *http.Request) {
 			}
 		}
 	}
+}
+
+func (s *server) generateHandler(w http.ResponseWriter, r *http.Request) {
+	ctx := appengine.NewContext(r)
+	log.Infof(ctx, "generate...")
+	for _, problemType := range []generator.Problem{
+		generator.ProblemType1,
+		generator.ProblemType3,
+	} {
+		if err := generateAndSave(ctx, problemType); err != nil {
+			log.Errorf(ctx, "failed to generate: %v", err)
+			http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+			return
+		}
+	}
+}
+
+// constant values
+const (
+	KindNameProblem = "Problem"
+	StockCount      = 50
+)
+
+type problemEntity struct {
+	State  string   `datastore:"state,noindex"`
+	Type   int      `datastore:"type"`
+	Used   bool     `datastore:"used"`
+	Answer []string `datastore:"answer,noindex"`
+}
+
+func generateAndSave(ctx context.Context, problemType generator.Problem) error {
+	count, err := datastore.NewQuery(KindNameProblem).
+		Filter("type = ", problemType.Steps()).
+		Filter("used = ", false).
+		Count(ctx)
+	if err != nil {
+		return err
+	}
+	log.Infof(ctx, "type: %v, count: %v", problemType.Steps(), count)
+	if count >= StockCount {
+		return nil
+	}
+	p := generator.Generate(problemType)
+	answer, err := solver.Solve(p)
+	if err != nil {
+		return err
+	}
+	entity := &problemEntity{
+		State:  csa.InitialState2(p),
+		Type:   len(answer),
+		Answer: answer,
+		Used:   false,
+	}
+	key, err := datastore.Put(ctx, datastore.NewIncompleteKey(ctx, KindNameProblem, nil), entity)
+	if err != nil {
+		return err
+	}
+	log.Infof(ctx, "key: %v", key)
+	return nil
+}
+
+func fetchProblem(ctx context.Context, problemType generator.Problem) (*problemEntity, error) {
+	query := datastore.NewQuery(KindNameProblem).
+		Filter("type = ", problemType.Steps())
+	iter := query.
+		Filter("used = ", false).
+		Run(ctx)
+	var entity problemEntity
+	key, err := iter.Next(&entity)
+	if err != nil {
+		if err != datastore.Done {
+			return nil, err
+		}
+		count, err := query.Count(ctx)
+		if err != nil {
+			return nil, err
+		}
+		if count == 0 {
+			return nil, datastore.Done
+		}
+		_, err = query.Offset(rand.Intn(count)).Run(ctx).Next(&entity)
+		if err != nil {
+			return nil, err
+		}
+	} else {
+		entity.Used = true
+		_, err = datastore.Put(ctx, key, &entity)
+		if err != nil {
+			return nil, err
+		}
+	}
+	return &entity, nil
 }

a8f2140c0eb3112647c2f570a597f5834b8fd15f
sugyan <sugi1982@gmail.com>
2017-11-19 19:30:46 +0900

    add Twitter bot

--- app.go
+++ app.go
@@ -3,25 +3,44 @@ package app
 import (
 	"bytes"
 	"context"
+	"encoding/base64"
 	"fmt"
+	"image/jpeg"
 	"math/rand"
 	"net/http"
+	"net/url"
 	"strings"
 
 	"github.com/BurntSushi/toml"
+	"github.com/ChimeraCoder/anaconda"
 	"github.com/line/line-bot-sdk-go/linebot"
 	"github.com/sugyan/shogi/format/csa"
 	"github.com/sugyan/shogi/logic/problem/generator"
 	"github.com/sugyan/shogi/logic/problem/solver"
+	"github.com/sugyan/shogi/util/image"
 	"google.golang.org/appengine"
 	"google.golang.org/appengine/datastore"
 	"google.golang.org/appengine/log"
 	"google.golang.org/appengine/urlfetch"
+	// just import only for appengine...
+	_ "github.com/ChimeraCoder/tokenbucket"
+	_ "github.com/azr/backoff"
+	_ "github.com/dustin/go-jsonpointer"
+	_ "github.com/dustin/gojson"
+	_ "github.com/garyburd/go-oauth/oauth"
 )
 
 type config struct {
-	ChannelSecret      string `toml:"channel_secret"`
-	ChannelAccessToken string `toml:"channel_access_token"`
+	LineBot struct {
+		ChannelSecret      string `toml:"channel_secret"`
+		ChannelAccessToken string `toml:"channel_access_token"`
+	} `toml:"line_bot"`
+	TwitterBot struct {
+		ConsumerKey       string `toml:"consumer_key"`
+		ConsumerSecret    string `toml:"consumer_secret"`
+		AccessToken       string `toml:"access_token"`
+		AccessTokenSecret string `toml:"access_token_secret"`
+	} `toml:"twitter_bot"`
 }
 
 type server struct {
@@ -34,17 +53,21 @@ func init() {
 	if err != nil {
 		panic(err)
 	}
+	anaconda.SetConsumerKey(config.TwitterBot.ConsumerKey)
+	anaconda.SetConsumerSecret(config.TwitterBot.ConsumerSecret)
+
 	server := &server{
 		config: config,
 	}
 	http.HandleFunc("/callback", server.botHandler)
 	http.HandleFunc("/generate", server.generateHandler)
+	http.HandleFunc("/tweet", server.tweetHandler)
 }
 
 func (s *server) botHandler(w http.ResponseWriter, r *http.Request) {
 	ctx := appengine.NewContext(r)
 	bot, err := linebot.New(
-		s.config.ChannelSecret, s.config.ChannelAccessToken,
+		s.config.LineBot.ChannelSecret, s.config.LineBot.ChannelAccessToken,
 		linebot.WithHTTPClient(urlfetch.Client(ctx)),
 	)
 	if err != nil {
@@ -133,6 +156,61 @@ func (s *server) generateHandler(w http.ResponseWriter, r *http.Request) {
 			return
 		}
 	}
+}
+
+func (s *server) tweetHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Header.Get("X-Appengine-Cron") != "true" {
+		return
+	}
+	// TODO: check cron header
+	ctx := appengine.NewContext(r)
+	log.Infof(ctx, "tweet...")
+	if err := s.tweetProblem(ctx); err != nil {
+		log.Errorf(ctx, "failed to tweet: %v", err)
+		http.Error(w, http.StatusText(http.StatusInternalServerError), http.StatusInternalServerError)
+		return
+	}
+}
+
+func (s *server) tweetProblem(ctx context.Context) error {
+	api := anaconda.NewTwitterApi(s.config.TwitterBot.AccessToken, s.config.TwitterBot.AccessTokenSecret)
+	api.HttpClient.Transport = &urlfetch.Transport{Context: ctx}
+
+	problemEntity, err := fetchProblem(ctx, generator.ProblemType3)
+	if err != nil {
+		return err
+	}
+	problem, err := csa.Parse(bytes.NewBufferString(problemEntity.State))
+	if err != nil {
+		return err
+	}
+	img, err := image.Generate(problem, &image.StyleOptions{
+		Board: image.BoardStripe,
+		Piece: image.PieceDirty,
+	})
+	if err != nil {
+		return err
+	}
+	buf := bytes.NewBuffer([]byte{})
+	// Error when sending PNG image...
+	if err := jpeg.Encode(base64.NewEncoder(base64.RawStdEncoding, buf), img, &jpeg.Options{
+		Quality: 90,
+	}); err != nil {
+		return err
+	}
+	media, err := api.UploadMedia(buf.String())
+	if err != nil {
+		return err
+	}
+	params := url.Values{}
+	params.Add("media_ids", media.MediaIDString)
+	status := fmt.Sprintf("%d手詰の問題です！", problemEntity.Type)
+	tweet, err := api.PostTweet(status, params)
+	if err != nil {
+		return err
+	}
+	log.Infof(ctx, "tweeted: %v", tweet.IdStr)
+	return nil
 }
 
 // constant values
